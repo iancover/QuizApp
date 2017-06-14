751eaeb5d3f11131432d0eba3553d51e08bb5a68
Ian Cover <iancover@gmail.com>
2017-06-14 16:14:28 -0500

    git add app.js index.html main.css

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+// $(function getElementById("answer").onclick = validate;);
+
+// function validate() {	    
+// 	var radios;	    
+// 	var i;	    
+// 	var correct;	    
+	
+// 	radios = getElementById("question1").getElementsByTagName("input"); 	    
+// 	correct = false;
+
+// 	for(i = 0; i < radios.length; i++) {		    
+// 		if(radios[i].value == "yes" && radios[i].checked == true) {			  
+// 			correct = true;		    
+// 		}	    
+// 	} 	    
+// 		if(correct) {		    
+// 			alert("Correct");	    
+// 				} 
+// 		else {		    
+// 			alert("Incorrect");	    
+// 			}    
+// 		}
